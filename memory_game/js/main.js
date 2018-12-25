8e1af4d50fe7ab4df09dff482a951ecd22469845
Meisam <pmeisam@ymail.com>
2018-12-25 15:23:12 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("up and running!");
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
